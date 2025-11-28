d4cc38a706ac18b14cc1bb8a8ca4deac25e0ee59
arturanalice6-cell <arturanalice6@gmail.com>
2025-11-28 15:33:57 -0300

    Update Separator.jsx

--- src/components/ui/Separator.jsx
+++ src/components/ui/Separator.jsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
-import { cn } from "@/lib/utils"
+import { cn } from "@/utils"
 
 const Separator = React.forwardRef((
   { className, orientation = "horizontal", decorative = true, ...props },
@@ -16,7 +15,8 @@ const Separator = React.forwardRef((
       orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
       className
     )}
-    {...props} />
+    {...props}
+  />
 ))
 Separator.displayName = SeparatorPrimitive.Root.displayName
 
