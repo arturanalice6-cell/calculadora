6ac782c8ea027d9d0c050243fc72d5a96ec1775a
arturanalice6-cell <arturanalice6@gmail.com>
2025-11-28 14:15:23 -0300

    Update Accordion.jsx

--- src/components/ui/Accordion.jsx
+++ src/components/ui/Accordion.jsx
@@ -1,13 +1,16 @@
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
-
-import { cn } from "@/lib/utils"
+import { cn } from "@/utils"
 
 const Accordion = AccordionPrimitive.Root
 
 const AccordionItem = React.forwardRef(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item ref={ref} className={cn("border-b", className)} {...props} />
+  <AccordionPrimitive.Item 
+    ref={ref} 
+    className={cn("border-b", className)} 
+    {...props} 
+  />
 ))
 AccordionItem.displayName = "AccordionItem"
 
@@ -19,10 +22,10 @@ const AccordionTrigger = React.forwardRef(({ className, children, ...props }, re
         "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline text-left [&[data-state=open]>svg]:rotate-180",
         className
       )}
-      {...props}>
+      {...props}
+    >
       {children}
-      <ChevronDown
-        className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
@@ -32,8 +35,11 @@ const AccordionContent = React.forwardRef(({ className, children, ...props }, re
   <AccordionPrimitive.Content
     ref={ref}
     className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
-    {...props}>
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    {...props}
+  >
+    <div className={cn("pb-4 pt-0", className)}>
+      {children}
+    </div>
   </AccordionPrimitive.Content>
 ))
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
